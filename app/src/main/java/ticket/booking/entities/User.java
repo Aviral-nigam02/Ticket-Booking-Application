45494197ccc216674d471c2a46054c5d51d9a054
Aviral Nigam <anigam917@gmail.com>
2025-03-12 23:10:05 +0530

    user.json setup

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -1,12 +1,73 @@
 package ticket.booking.entities;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true) // Ignore unexpected JSON fields
 public class User {
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("password")
     private String password;
+
+    @JsonProperty("hashedPassword")
     private String hashedPassword;
+
+    @JsonProperty("ticketsBooked")
     private List<Ticket> ticketsBooked;
+
+    @JsonProperty("user_id")  // ✅ Map "user_id" from JSON to "userId" in Java
     private String userId;
 
+    // ✅ No-argument constructor for Jackson
+    public User() {
+    }
+
+    public User(String name, String password, String hashedPassword, List<Ticket> ticketsBooked, String userId) {
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.ticketsBooked = ticketsBooked;
+        this.userId = userId;
+    }
+
+    // ✅ Getters and Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public List<Ticket> getTicketsBooked() {
+        return ticketsBooked;
+    }
+    public void setTicketsBooked(List<Ticket> ticketsBooked) {
+        this.ticketsBooked = ticketsBooked;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void printTickets() {
+    }
 }
